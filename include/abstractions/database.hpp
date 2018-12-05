f3cda772764329a28763c7a9cc94c65b26e02132
Daniel Krawisz <daniel.krawisz@thingobjectentity.net>
2018-12-04 23:58:54 -0600

    database object which can keep track of relationship between keys and tags.

--- include/abstractions/database.hpp
+++ include/abstractions/database.hpp
@@ -1,32 +1,83 @@
 #ifndef ABSTRACTIONS_DATABASE_HPP
 #define ABSTRACTIONS_DATABASE_HPP
 
+#include "key.hpp"
+#include "map.hpp"
+
 namespace abstractions
 {
-
-    template<typename tag>
-    struct memory {
-        virtual bool exists(tag) const = 0;
-    };
+    
+    namespace database
+    {
         
-    template <typename K>
-    struct entry {
-        bool Exists;
-        K Value;
+        template <typename to_public, typename secret, typename pubkey, typename F, typename tag>
+        struct tagged_key {
+            F Function;
             
-        entry() : Exists(false), Value() {}
-        entry(K k) : Exists(true), Value(k) {}
-    };
-
-    template<typename tag, typename K>
-    struct database : public memory<tag> {
-        virtual entry<K> get(tag) const = 0;
+            // The tag which is is produced by the public key. 
+            tag Tag;
             
-        bool exists(tag t) const final override {
-            return get(t).Exists;
+            // the keypair
+            key::pair<to_public, secret, pubkey> Keypair;
+            
+            key::proof_exists_private<to_public, secret, pubkey> KeyProof;
+            
+            inverse::proof<F, pubkey, tag> TagProof;
+            
+            bool validate() const {
+                return Function == TagProof.Exist.Function
+                    && Tag == TagProof.Proposition
+                    && Keypair.ToPublic == KeyProof.Exist.Function
+                    && Keypair.Pubkey == KeyProof.Proposition
+                    && Keypair.Pubkey == TagProof.Derivation
+                    && Keypair.Secret == KeyProof.Derivation;
+            }
+            
+            bool verify() const {
+                return validate() && KeyProof.verify() && TagProof.verify();
+            }
+            
         };
-    };
-
+        
+        template <typename Mtp, typename Mps, typename tag, typename pubkey, typename secret, typename F, typename to_public>
+        struct tag_map {
+            static const map::definition::map<Mtp, tag, inverse::proof<F, pubkey, tag>> r1{};
+            static const map::definition::map<Mps, pubkey, key::proof_exists_private<to_public, secret, pubkey>> r2{};
+            
+            static const map::definition::map<tag_map, tag, tagged_key<to_public, secret, pubkey, F, tag>> r3{};
+            
+            F Function;
+            
+            to_public ToPublic;
+            
+            Mtp TagsToPubkeys;
+            
+            Mps PubkeysToPrivkeys;
+        
+            bool empty() const {
+                return map::empty(TagsToPubkeys);
+            }
+            
+            tagged_key<to_public, secret, pubkey, F, tag> operator[](tag t) {
+                return inverse::prove(Function, k, Map[k]);
+            }
+            
+            tag_map insert(key k, inverse::proof<F, key, value> p) {
+                if (k != p.SuchThat && p.Exist.Function != Function) return {};
+                return {Function, Map.insert(k, p)};
+            }
+            
+            tag_map insert(key k) {
+                return {Function, Map.insert(k, Function(k))};
+            }
+            
+            tag_map remove(tag t) {
+                return {Function, ToPublic, TagsToPubkeys, Map.remove(k)};
+            }
+        };
+        
+    }
+    
 }
 
 #endif
