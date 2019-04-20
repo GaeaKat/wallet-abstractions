c499507eec63e58fb1105e98904721b1feccadf4
Daniel Krawisz <daniel.krawisz@thingobjectentity.net>
2019-04-20 15:44:03 -0500

    f

--- include/abstractions/pattern.hpp
+++ include/abstractions/pattern.hpp
@@ -7,69 +7,151 @@
 namespace abstractions {
     
     namespace pattern {
-    
-        template <
-            typename Key,
-            typename Script, 
-            typename Tx, 
-            typename Interpreter
-        > struct basic : 
-            public machine::definition<Interpreter, Script, Tx> {
-            
-            // make a script pubkey. 
-            virtual Script pay(Key) const = 0;
-            
-            // make a script signature.
-            virtual Script redeem(Key, Tx) const = 0;
-            
-            void pattern_definition(Key k, Tx t, Interpreter i) {
-                assert(i.run(pay(k), redeem(k, t), t));
-            }
-        };
-    
-        template <
-            typename Key,
-            typename Script, 
-            typename Data, 
-            typename Tx, 
-            typename Interpreter
-        > struct tagged : public basic<Key, Interpreter, Script, Tx> {
-            
-            virtual Data tag(Key) const = 0;
-            
-        };
-    
-        template <
-            typename Key,
-            typename Script, 
-            typename Data, 
-            typename Tx, 
-            typename Interpreter
-        > struct recognizable : public tagged<Key, Interpreter, Data, Script, Tx> {
+        
+        namespace abstract {
+        
+            template <
+                typename Key,
+                typename Script, 
+                typename Tx, 
+                typename Machine
+            > struct basic : 
+                public virtual machine::definition<Machine, Script, Tx> {
                 
-            virtual bool recognize(Script, Data) const = 0;
+                // make a script pubkey. 
+                virtual Script pay(Key) const = 0;
                 
-            void recognizable_pattern_definition(Key k) {
-                assert(recognize(basic<Key, Interpreter, Script, Tx>::pay(k),
-                                tagged<Key, Interpreter, Data, Script, Tx>::tag(k)));
-            }
+                // make a script signature.
+                virtual Script redeem(Key, Tx) const = 0;
+                
+                void pattern_definition(Key k, Tx t, Machine i) const final {
+                    assert(i.run(pay(k), redeem(k, t), t));
+                }
+            };
+        
+            template <
+                typename Key,
+                typename Script, 
+                typename Tag, 
+                typename Tx, 
+                typename Machine
+            > struct tagged : public virtual basic<Key, Machine, Script, Tx> {
+                
+                virtual Tag tag(Key) const = 0;
+                
+            };
+        
+            template <
+                typename Key,
+                typename Script, 
+                typename Tag, 
+                typename Tx, 
+                typename Machine
+            > struct recognizable : public virtual tagged<Key, Machine, Tag, Script, Tx> {
+                
+                virtual bool recognize(Script, Tag) const = 0;
+                
+                void recognizable_pattern_definition(Key k) const final {
+                    assert(recognize(basic<Key, Machine, Script, Tx>::pay(k),
+                                     tagged<Key, Machine, Tag, Script, Tx>::tag(k)));
+                }
+                
+            };
+        
+            template <
+                typename Key,
+                typename Script, 
+                typename Tag, 
+                typename Tx, 
+                typename Machine
+            > struct addressable : public virtual tagged<Key, Machine, Tag, Script, Tx> {
+                
+                virtual Script pay(Tag) const = 0;
+                
+                Script pay(Key k) const final override {
+                    return pay(tagged<Key, Machine, Tag, Script, Tx>::tag(k));
+                }
+                
+            };
             
-        };
-    
+            template <
+                typename Sk,
+                typename Pk, 
+                typename Script, 
+                typename Tag, 
+                typename Tx, 
+                typename Machine
+            > struct standard : public virtual data::crypto::keypair<Sk, Pk>, 
+                public recognizable<Sk, Machine, Tag, Script, Tx>, 
+                public addressable<Sk, Machine, Tag, Script, Tx> {
+                    
+                virtual Tag tag(Pk) const = 0;
+                
+                Tag tag(Sk k) const final override {
+                    return tag(data::crypto::to_public<Sk, Pk>{}(k));
+                };
+            };
+            
+        }
+        
         template <
-            typename Key,
+            typename Sk, 
+            typename Pk, 
             typename Script, 
-            typename Data, 
+            typename Pay, 
+            typename Redeem, 
+            typename Recognize, 
             typename Tx, 
-            typename Interpreter
-        > struct addressable : public tagged<Key, Interpreter, Data, Script, Tx> {
+            typename Machine
+        > struct pay_to_public_key final : public abstract::standard<Sk, Pk, Script, Pk, Tx, Machine> {
             
-            virtual Script pay(Data) const = 0;
+            Pk tag(Pk k) const final override {
+                return k;
+            };
             
-            void recognizable_pattern_definition(Key k) {
-                assert(pay(tagged<Key, Interpreter, Data, Script, Tx>::tag(k)) == basic<Key, Interpreter, Script, Tx>::pay(k));
+            Script pay(Pk k) const final override {
+                return Pay{}(k);
+            };
+            
+            Script redeem(Sk k, Tx t) const final override {
+                return Redeem{}(k, t);
             }
             
+            bool recognize(Script s, Pk k) const final override {
+                return Recognize{}(s, k);
+            };
+        
+        };
+        
+        template <
+            typename Sk, 
+            typename Pk, 
+            typename Script, 
+            typename Address, 
+            typename Pay, 
+            typename Redeem, 
+            typename Recognize, 
+            typename Hash, 
+            typename Tx, 
+            typename Machine
+        > struct pay_to_address final : public abstract::standard<Sk, Pk, Script, Address, Tx, Machine> {
+            
+            Address tag(Pk k) const final override {
+                return Hash{}(k);
+            };
+            
+            Script pay(Address a) const final override {
+                return Pay{}(a);
+            };
+            
+            Script redeem(Sk k, Tx t) const final override {
+                return Redeem{}(k, t);
+            }
+            
+            bool recognize(Script s, Hash k) const final override {
+                return Recognize{}(s, k);
+            };
+        
         };
         
     }
