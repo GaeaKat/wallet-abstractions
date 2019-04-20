74ac5ddd59279aa30874da1d1b194fdd20ffef9d
Daniel Krawisz <daniel.krawisz@thingobjectentity.net>
2019-04-20 13:27:29 -0500

    Redo pattern again

--- include/abstractions/pattern.hpp
+++ include/abstractions/pattern.hpp
@@ -5,24 +5,74 @@
 #include <abstractions/machine.hpp>
 
 namespace abstractions {
+    
+    namespace pattern {
+    
+        template <
+            typename Key,
+            typename Script, 
+            typename Tx, 
+            typename Interpreter
+        > struct basic : 
+            public machine::definition<Interpreter, Script, Tx> {
+            
+            // make a script pubkey. 
+            virtual Script pay(Key) const = 0;
+            
+            // make a script signature.
+            virtual Script redeem(Key, Tx) const = 0;
+            
+            void pattern_definition(Key k, Tx t, Interpreter i) {
+                assert(i.run(pay(k), redeem(k, t), t));
+            }
+        };
+    
+        template <
+            typename Key,
+            typename Script, 
+            typename Data, 
+            typename Tx, 
+            typename Interpreter
+        > struct tagged : public basic<Key, Interpreter, Script, Tx> {
+            
+            virtual Data tag(Key) const = 0;
+            
+        };
+    
+        template <
+            typename Key,
+            typename Script, 
+            typename Data, 
+            typename Tx, 
+            typename Interpreter
+        > struct recognizable : public tagged<Key, Interpreter, Data, Script, Tx> {
+                
+            virtual bool recognize(Script, Data) const = 0;
+                
+            void recognizable_pattern_definition(Key k) {
+                assert(recognize(basic<Key, Interpreter, Script, Tx>::pay(k),
+                                tagged<Key, Interpreter, Data, Script, Tx>::tag(k)));
+            }
+            
+        };
+    
+        template <
+            typename Key,
+            typename Script, 
+            typename Data, 
+            typename Tx, 
+            typename Interpreter
+        > struct addressable : public tagged<Key, Interpreter, Data, Script, Tx> {
+            
+            virtual Script pay(Data) const = 0;
+            
+            void recognizable_pattern_definition(Key k) {
+                assert(pay(tagged<Key, Interpreter, Data, Script, Tx>::tag(k)) == basic<Key, Interpreter, Script, Tx>::pay(k));
+            }
+            
+        };
         
-    template <
-        typename pk,
-        typename sk,
-        typename script, 
-        typename pay,
-        typename redeem, 
-        typename tx, 
-        typename interpreter
-    > struct pattern : 
-        public data::crypto::keypair<pk, sk>, 
-        public data::function::definition<pay, pk, script>, 
-        public machine::definition<interpreter, script, tx> {
-        
-        static void required(pk pubkey, sk secret, pay p, redeem r, tx t, interpreter i) {
-            assert(i.run(p(pubkey), r(secret, t), t));
-        }
-    };
+    }
     
 } 
 
